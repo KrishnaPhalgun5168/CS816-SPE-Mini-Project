c227537ab6dbebd3680dc7956413906a52de022d
KrishnaPhalgun5168 <59023719+KrishnaPhalgun5168@users.noreply.github.com>
2022-04-17 13:04:37 +0530

    updated ansible

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo "Ansible Entrypoint"
+
+echo "This is the secret: $SSH_PASSWORD"
+
+echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
+
+ansible-playbook ansible/playbook.yml --user ubuntu
